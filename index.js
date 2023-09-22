c53ec8f7c8fc4c3fa929ad026f5a55c48edc173d
josharsh <harsh.joshi.pth@gmail.com>
2023-09-23 02:46:20 +0530

    Final Changes to Logger

--- index.js
+++ index.js
@@ -1,7 +1,68 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const logger_1 = __importDefault(require("./logger"));
-exports.default = logger_1.default;
+exports.setCurrentRoute = void 0;
+const storage_1 = require("@google-cloud/storage");
+class LogicalLogger {
+    constructor() {
+        this.currentRoute = '';
+        // log(route: string, message: any, ...optionalParams: any[]) {
+        //     const logMessage = `[LogicalLog] ${message} ${optionalParams.join(' ')}`;
+        //     console.log(logMessage);
+        //     this.writeToGCS(route, logMessage);
+        // }
+    }
+    // constructor(bucketName: string, keyFilename: string) {
+    //     this.storage = new Storage({ keyFilename });
+    //     this.bucketName = bucketName;
+    // }
+    initialize(bucketName, keyFilename) {
+        this.storage = new storage_1.Storage({ keyFilename });
+        this.bucketName = bucketName;
+    }
+    setCurrentRoute(route) {
+        this.currentRoute = route;
+    }
+    writeToGCS(message) {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const date = new Date().toISOString();
+                const fileName = `${date}.log`;
+                console.log(`LogiLog file name is ${fileName}`);
+                const file = this.storage.bucket(this.bucketName).file(`${this.currentRoute}/${fileName}`);
+                console.log(`LogiLog file saved in ${file}`);
+                yield file.save(message);
+            }
+            catch (error) {
+                console.error('[LogicLog Error] Failed to write to GCS:', error);
+            }
+        });
+    }
+    log(message, ...optionalParams) {
+        const logMessage = `[LogicLog] ${message} ${optionalParams.join(' ')}`;
+        console.log(logMessage);
+        console.log("[LogiLog] Current route has been set to ", this.currentRoute);
+        if (this.currentRoute) {
+            this.writeToGCS(logMessage);
+        }
+    }
+}
+const logilogger = new LogicalLogger();
+function setCurrentRoute(req, res, next) {
+    const route = req.path.replace(/\/:[a-zA-Z0-9_]+/g, (match) => {
+        const paramKey = match.slice(2); // remove "/:"
+        return `/${req.params[paramKey]}`;
+    });
+    logilogger.setCurrentRoute(route);
+    next();
+}
+exports.setCurrentRoute = setCurrentRoute;
+exports.default = logilogger;
--- index.ts
+++ index.ts
@@ -1,2 +1,64 @@
-export { default as logilogger } from "./logger";
-export { setCurrentRoute } from "./logger";
+import { Storage } from '@google-cloud/storage';
+import { Request, Response, NextFunction } from 'express';
+
+class LogicalLogger {
+    private storage?: Storage;
+    private bucketName?: string;
+    private currentRoute: string = '';
+
+    // constructor(bucketName: string, keyFilename: string) {
+    //     this.storage = new Storage({ keyFilename });
+    //     this.bucketName = bucketName;
+    // }
+
+    initialize(bucketName: string, keyFilename: string) {
+        this.storage = new Storage({ keyFilename });
+        this.bucketName = bucketName;
+    }
+
+    setCurrentRoute(route: string) {
+        this.currentRoute = route;
+    }
+    
+    private async writeToGCS(message: string) {
+        try {
+            const date = new Date().toISOString();
+            const fileName = `${date}.log`;
+            console.log(`LogiLog file name is ${fileName}`)
+            const file = this.storage!.bucket(this.bucketName!).file(`${this.currentRoute}/${fileName}`);
+            console.log(`LogiLog file saved in ${file}`)
+            await file.save(message);
+        }
+        catch (error) {
+            console.error('[LogicLog Error] Failed to write to GCS:', error);
+        }
+    }
+
+    log(message: any, ...optionalParams: any[]) {
+        const logMessage = `[LogicLog] ${message} ${optionalParams.join(' ')}`;
+        console.log(logMessage);
+        console.log("[LogiLog] Current route has been set to ", this.currentRoute)
+        if (this.currentRoute) {
+            this.writeToGCS(logMessage);
+        }
+    }
+    // log(route: string, message: any, ...optionalParams: any[]) {
+    //     const logMessage = `[LogicalLog] ${message} ${optionalParams.join(' ')}`;
+    //     console.log(logMessage);
+    //     this.writeToGCS(route, logMessage);
+    // }
+}
+
+const logilogger = new LogicalLogger();
+
+export function setCurrentRoute(req: Request, res: Response, next: NextFunction) {
+    const route = req.path.replace(/\/:[a-zA-Z0-9_]+/g, (match) => {
+        const paramKey = match.slice(2); // remove "/:"
+        return `/${req.params[paramKey]}`;
+    });
+
+    logilogger.setCurrentRoute(route);
+    next();
+}
+
+export default logilogger;
